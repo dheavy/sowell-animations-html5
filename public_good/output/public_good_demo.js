5075e65191cbd278aa4749fd636cd6fdd0b470b9
Davy Peter Braun <hello@davybraun.com>
2016-02-26 11:55:06 +0100

    Create simulation version of public good game.

--- public_good/output/public_good_demo.js
+++ public_good/output/public_good_demo.js
@@ -1339,53 +1339,119 @@ p.nominalBounds = new cjs.Rectangle(-17.5,-8.6,35,17.3);
 
 
 // stage content:
-(lib.public_good_demo = function(mode,startPosition,loop) {
-	this.initialize(mode,startPosition,loop,{init:0,start:35});
+(lib.public_good_simulation = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{init:35,start:36});
 
 	// timeline functions:
 	this.frame_35 = function() {
 		/*
 		 Process
 		 -------
-		 Find if canvas in HTML shell page has the following data-attributes:
-		 - `data-estimation-min`
-		 - `data-estimation-max`
-		 - `data-estimation-currency`
-		 - `data-multipler`
+		 Module pattern opening an API accessible via window.publicGood namespace to interact
+		 withe current animation from outside of it.
 		 
-		 Use them as attributes in the animation if found.
-		 Use default attributes if not.
-		 
-		 Assumes there is only on canvas in the page.
+		 `currency`, `multiplier` variables can be set via data-attributes, respectively
+		 `data-currency` and `data-multiplier`. They revert to defaults if not set.
 		 */
-		 
-		var min = 5;
-		var max = 10;
+		
+		this.stop()
+		
 		var currency = '€';
 		var multiplier = 1.6;
 		
 		if (document) {
 			var canvas = document.getElementsByTagName('canvas')[0];
 			if (canvas) {
-				min = canvas.dataset.estimationMin || min;
-				max = canvas.dataset.estimationMax || max;
-				currency = canvas.dataset.estimationCurrency || currency;
+				currency = canvas.dataset.currency || currency;
 				multiplier = canvas.dataset.multiplier|| multiplier;
 			}
 		}
 		
-		function getRandomInt(min, max) {
-			return Math.floor(Math.random() * (max - min)) + min;
-		}
-		
-		this.bubble1.label.text = getRandomInt(min, max).toString() + currency;
-		this.bubble2.label.text = getRandomInt(min, max).toString() + currency;
-		this.bubble3.label.text = getRandomInt(min, max).toString() + currency;
-		this.bubble4.label.text = getRandomInt(min, max).toString() + currency;
 		this.bubbleMultiplier.label.text = '×' + multiplier;
+		
+		var self = this; // Store reference to 'lib.public_good_simulation' -> the current animation object.
+		
+		window.publicGood = (function() {
+			var playersAmount = [0, 0, 0, 0];
+			
+			function getPlayerBubble(index) {
+				return self['bubble' + index];
+			}
+			
+			function incrementAmount(player) {
+				playersAmount[player]++;
+				getPlayerBubble(player+1).label.text = playersAmount[player].toString() + currency;
+			}
+			
+			function decrementAmount(player) {
+				playersAmount[player]--;
+				getPlayerBubble(player+1).label.text = playersAmount[player].toString() + currency;
+			}
+		
+			function incrementAmountPlayer1() {
+				incrementAmount(0);
+			}
+			
+			function incrementAmountPlayer2() {
+				incrementAmount(1);
+			}
+			
+			function incrementAmountPlayer3() {
+				incrementAmount(2);
+			}
+			
+			function incrementAmountPlayer4() {
+				incrementAmount(3);
+			}
+			
+			function decrementAmountPlayer1() {
+				decrementAmount(0);
+			}
+			
+			function decrementAmountPlayer2() {
+				decrementAmount(1);
+			}
+			
+			function decrementAmountPlayer3() {
+				decrementAmount(2);
+			}
+			
+			function decrementAmountPlayer4() {
+				decrementAmount(3);
+			}
+			
+			function start() {
+				self.gotoAndPlay('start');
+			}
+			
+			function reset() {
+				playersAmount = [0, 0, 0, 0];
+				for (var i = 0, len = playersAmount.length; i < len; i++) {
+					getPlayerBubble(i+1).label.text = playersAmount[i].toString() + currency;
+				}
+			}
+			
+			reset();
+			
+			return {
+				incrementAmountPlayer1: incrementAmountPlayer1,
+				incrementAmountPlayer2: incrementAmountPlayer2,
+				incrementAmountPlayer3: incrementAmountPlayer3,
+				incrementAmountPlayer4: incrementAmountPlayer4,
+				
+				decrementAmountPlayer1: decrementAmountPlayer1,
+				decrementAmountPlayer2: decrementAmountPlayer2,
+				decrementAmountPlayer3: decrementAmountPlayer3,
+				decrementAmountPlayer4: decrementAmountPlayer4,
+				
+				start: start,
+				reset: reset
+			}
+			
+		}());
 	}
 	this.frame_414 = function() {
-		this.gotoAndPlay('start');
+		this.gotoAndPlay('init');
 	}
 
 	// actions tween:
@@ -1433,31 +1499,35 @@ p.nominalBounds = new cjs.Rectangle(-17.5,-8.6,35,17.3);
 
 	// bubble 4
 	this.bubble4 = new lib.bubbleRed();
-	this.bubble4.setTransform(547.6,321.7,0.099,0.084,0,0,32,38.3,41.9);
+	this.bubble4.setTransform(539.6,305.7,0.786,0.667,0,0,32,38.3,41.8);
+	this.bubble4.alpha = 0;
 	this.bubble4._off = true;
 
-	this.timeline.addTween(cjs.Tween.get(this.bubble4).wait(50).to({_off:false},0).to({regY:41.8,scaleX:0.79,scaleY:0.67,x:539.6,y:305.7},4,cjs.Ease.get(1)).wait(42).to({regY:41.9,scaleX:0.1,scaleY:0.08,x:547.6,y:321.7},3,cjs.Ease.get(1)).to({_off:true},1).wait(315));
+	this.timeline.addTween(cjs.Tween.get(this.bubble4).wait(35).to({_off:false},0).wait(15).to({regY:41.9,scaleX:0.1,scaleY:0.08,x:547.6,y:321.7,alpha:1},0).to({regY:41.8,scaleX:0.79,scaleY:0.67,x:539.6,y:305.7},4,cjs.Ease.get(1)).wait(42).to({regY:41.9,scaleX:0.1,scaleY:0.08,x:547.6,y:321.7},3,cjs.Ease.get(1)).to({_off:true},1).wait(315));
 
 	// bubble 3
 	this.bubble3 = new lib.bubbleBlue();
-	this.bubble3.setTransform(476.6,278.6,0.167,0.141,0,0,32,38.2,42);
+	this.bubble3.setTransform(460.6,254.5,0.786,0.667,0,0,32,38.3,41.8);
+	this.bubble3.alpha = 0;
 	this.bubble3._off = true;
 
-	this.timeline.addTween(cjs.Tween.get(this.bubble3).wait(50).to({_off:false},0).to({regX:38.3,regY:41.8,scaleX:0.79,scaleY:0.67,x:460.6,y:254.5},4,cjs.Ease.get(1)).wait(42).to({regX:38.2,regY:42,scaleX:0.17,scaleY:0.14,x:476.6,y:278.6},3,cjs.Ease.get(1)).to({_off:true},1).wait(315));
+	this.timeline.addTween(cjs.Tween.get(this.bubble3).wait(35).to({_off:false},0).wait(15).to({regX:38.2,regY:42,scaleX:0.17,scaleY:0.14,x:476.6,y:278.6,alpha:1},0).to({regX:38.3,regY:41.8,scaleX:0.79,scaleY:0.67,x:460.6,y:254.5},4,cjs.Ease.get(1)).wait(42).to({regX:38.2,regY:42,scaleX:0.17,scaleY:0.14,x:476.6,y:278.6},3,cjs.Ease.get(1)).to({_off:true},1).wait(315));
 
 	// bubble 2
 	this.bubble2 = new lib.bubbleOrange();
-	this.bubble2.setTransform(400.1,231.9,0.147,0.124,0,0,32,38.2,42.1);
+	this.bubble2.setTransform(376.2,199.8,0.786,0.667,0,0,32,38.3,41.8);
+	this.bubble2.alpha = 0;
 	this.bubble2._off = true;
 
-	this.timeline.addTween(cjs.Tween.get(this.bubble2).wait(50).to({_off:false},0).to({regX:38.3,regY:41.8,scaleX:0.79,scaleY:0.67,x:376.2,y:199.8},4,cjs.Ease.get(1)).wait(42).to({regX:38.2,regY:42.1,scaleX:0.15,scaleY:0.12,x:400.1,y:231.9},3,cjs.Ease.get(1)).to({_off:true},1).wait(315));
+	this.timeline.addTween(cjs.Tween.get(this.bubble2).wait(35).to({_off:false},0).wait(15).to({regX:38.2,regY:42.1,scaleX:0.15,scaleY:0.12,x:400.1,y:231.9,alpha:1},0).to({regX:38.3,regY:41.8,scaleX:0.79,scaleY:0.67,x:376.2,y:199.8},4,cjs.Ease.get(1)).wait(42).to({regX:38.2,regY:42.1,scaleX:0.15,scaleY:0.12,x:400.1,y:231.9},3,cjs.Ease.get(1)).to({_off:true},1).wait(315));
 
 	// bubble 1
 	this.bubble1 = new lib.bubbleGreen();
-	this.bubble1.setTransform(193.4,104.3,0.139,0.118,0,0,32,38.2,42);
+	this.bubble1.setTransform(169.4,80.2,0.786,0.667,0,0,32,38.3,41.8);
+	this.bubble1.alpha = 0;
 	this.bubble1._off = true;
 
-	this.timeline.addTween(cjs.Tween.get(this.bubble1).wait(50).to({_off:false},0).to({regX:38.3,regY:41.8,scaleX:0.79,scaleY:0.67,x:169.4,y:80.2},4,cjs.Ease.get(1)).wait(42).to({regX:38.2,regY:42,scaleX:0.14,scaleY:0.12,x:193.4,y:104.3},3,cjs.Ease.get(1)).to({_off:true},1).wait(315));
+	this.timeline.addTween(cjs.Tween.get(this.bubble1).wait(35).to({_off:false},0).wait(15).to({regX:38.2,regY:42,scaleX:0.14,scaleY:0.12,x:193.4,y:104.3,alpha:1},0).to({regX:38.3,regY:41.8,scaleX:0.79,scaleY:0.67,x:169.4,y:80.2},4,cjs.Ease.get(1)).wait(42).to({regX:38.2,regY:42,scaleX:0.14,scaleY:0.12,x:193.4,y:104.3},3,cjs.Ease.get(1)).to({_off:true},1).wait(315));
 
 	// factory front
 	this.instance_4 = new lib.factory();
